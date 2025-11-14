fc66d5a1f66731883be195b61f2e3e2c1a2207df
Abhishek gupta <abhishekspps825406@gmail.com>
2025-11-14 09:53:29 +0530

    Data types and numbers

---
+++ Day01_Data_Types/index.js
@@ -0,0 +1,22 @@
+const PI  = Math.PI
+console.log(PI)
+
+
+// Rounding to the closest number
+// if above .5  up if less 0.5 down rounding
+
+console.log(Math.round(PI))             // 3 to round values to the nearest number
+
+console.log(Math.round(9.81))           // 10
+
+console.log(Math.floor(PI))             // 3 rounding down
+
+console.log(Math.ceil(PI))              // 4 rounding up
+
+console.log(Math.max(-1,3,4,2,-45,23)); // 23 , returns the maximum number
+
+console.log(Math.min(-1,3,4,2,-45,23)); // -45 , returns the minimum number
+
+console.log(Math.random())              // random number between 0 and 1
+
+console.log(Math.floor(Math.random()*11))              // random number between 0 and 10
